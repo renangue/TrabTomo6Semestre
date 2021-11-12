1a5916d6bed24d250bc32b58606d5199f09cd6d8
Allan Ramos Arôxa <79292104+AllanRamos-hue@users.noreply.github.com>
2021-11-11 22:39:42 -0300

    Update MainMenu.cs

--- TrabTomo6SemestreProject/Assets/Scripts/MainMenu.cs
+++ TrabTomo6SemestreProject/Assets/Scripts/MainMenu.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-    {
-        public void PlayGame ()
+{
+    public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-
 }
     
     
